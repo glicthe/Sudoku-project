6c2b0d4e482e49240df32ff0d59ae2aa3bfc2537
Praditama Hasan <praditamaajmal@gmail.com>
2024-12-06 20:22:35 +0700

    Update main.c

--- TUGAS BESAR SUDOKU/main.c
+++ TUGAS BESAR SUDOKU/main.c
@@ -1,58 +1,77 @@
+/*------------------------PROGRAM MAIN SUDOKU------------------------*/
+
+/*-------header--------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "GameSudoku.h"
 #include "Menu.h"
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+/*------------------------PROGRAM------------------------*/
 int main(int argc, char *argv[]) {
-	    int choice;
 
-    // Loop untuk menampilkan menu sampai user memilih "Exit"
+	//Kamus data		
+	int choice;
+	
+	// Loop untuk menampilkan menu sampai user memilih "Exit"
     while (1) {
-        int opsi;
-        kursorOut(71, 10);
-        printf("=== MENU UTAMA SUDOKU ===\n");
+    	bannerSudoku(50,2);		//Memanggil modul bannerSudoku
+        kursorOut(71, 9);		//Memanggil modul kursorOut
+        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 
+		201, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 
+		205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 187);
+        kursorOut(71,10);
+        setTextColor(12);
+        printf("   WELCOME TO OUR GAME  ");
+        setTextColor(15);
+        kursorOut(71,10);
+        printf("%c",186);
+        kursorOut(95,10);
+        printf("%c",186);
         kursorOut(71, 11);
-        printf("1. Start\n");
-        kursorOut(71, 12);
-        printf("2. Load Game\n");
-        kursorOut(71, 13);
-        printf("3. Highscore\n");
-        kursorOut(71, 14);
-        printf("4. Exit\n");
-        kursorOut(71, 15);
+        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 
+		200, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 
+		205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 205, 188);
+        setTextColor(15);
+        kursorOut(72, 13);
+        printf("1. Start");
+        kursorOut(72, 15);
+        printf("2. Load Game");
+        kursorOut(72, 17);
+        printf("3. Highscore");
+        kursorOut(72, 19);
+        printf("4. Exit");
+        kursorOut(72, 21);
         printf("Pilih opsi (1-4): ");
         scanf("%d", &choice);
-        getchar(); // Untuk menangani enter setelah input angka
-        system("cls"); // Membersihkan layar setiap kali menu ditampilkan
+        getchar(); 			// Scanf untuk menangani enter setelah input angka
+        system("cls"); 			// Membersihkan layar setiap kali menu ditampilkan
 
-        switch (choice) {
+        switch (choice) {		//Selection berdasarkan input user
             case 1:
-                opsi = 1;
-                startGame(opsi);
-                system("cls"); // Bersihkan layar sebelum masuk ke menu Start
+                
+                startGame();		//Memanggil modul startGame
+                system("cls");	
                 break;
                 
             case 2:
-                opsi = 2;
-                loadGame(opsi);
-                system("cls"); // Bersihkan layar sebelum masuk ke Load Game
+                
+                loadGame();		//Memanggil modul loadGame
+                system("cls"); 
                 break;
             case 3:
-                opsi = 3;
-                highscore(opsi);
-                system("cls"); // Bersihkan layar sebelum masuk ke Highscore
+                
+                highScore();		//Memanggil modul highScore
+                system("cls"); 
                 break;
             case 4:
-                exitGame();
+                exitGame();		//Memanggil modul exitGame
                 return 0;
             default:
                 printf("Pilihan tidak valid! Silakan pilih lagi.\n");
-                getchar(); // Menunggu user menekan enter sebelum kembali ke menu
+                getchar(); 
         }
     }
-			
 	
 	return 0;
 }
-
+/*------------------------END PROGRAM------------------------*/
